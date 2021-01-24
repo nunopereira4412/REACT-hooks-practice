94b784a8bafaf70b9ca843d67ce78414056e098d
Nuno Pereira <nuno.pereira4412@gmail.com>
2021-01-24 13:58:23 +0000

    Setting up state with useState, showing ingredients list and deleting ingredients from the list enabled

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+
+  const [inputTitle, setInputTitle]   = useState("");
+  const [inputAmount, setInputAmount] = useState("");
+
+
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    props.addIngredient({title: inputTitle, amount: inputAmount});
   };
 
   return (
@@ -15,11 +20,21 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input 
+              type="text" 
+              id="title" 
+              value={inputTitle}
+              onChange={event => setInputTitle(event.target.value)}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input 
+              type="number" 
+              id="amount" 
+              value={inputAmount} 
+              onChange={event => setInputAmount(event.target.value)}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientsList from './IngredientList';
 
 function Ingredients() {
+
+  const [ingredients, setIngredients] = useState([]);
+
+  const addIngredientHandler = (ing) => {
+    setIngredients(prevIngredients => [
+      ...prevIngredients, 
+      {
+        ...ing, 
+        id: Math.random().toString()
+      }
+    ])
+  };
+
+  const removeItemHandler = (id) => {
+    setIngredients(prevIngredients => prevIngredients.filter(ing => ing.id !== id));
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm addIngredient={addIngredientHandler}/>
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientsList ingredients={ingredients} onRemoveItem={removeItemHandler}/>
       </section>
     </div>
   );
