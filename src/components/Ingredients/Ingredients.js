3d1f15987d33c209d9af2a05505c33f00c3b592f
Nuno Pereira <nuno.pereira4412@gmail.com>
2021-01-24 17:47:03 +0000

    Filtering ingredients from the list

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,33 +1,45 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientsList from './IngredientList';
 
-function Ingredients() {
+const Ingredients = () => {
 
   const [ingredients, setIngredients] = useState([]);
 
-  const addIngredientHandler = (ing) => {
-    setIngredients(prevIngredients => [
-      ...prevIngredients, 
-      {
-        ...ing, 
-        id: Math.random().toString()
-      }
-    ])
+  useEffect(() => {
+    console.log("RENDERING INGREDIENTS");
+  }, [ingredients]);
+
+  const addIngredientHandler = ing => {
+    fetch("https://react-hooks-practice-64697-default-rtdb.firebaseio.com/ingredients.json", {
+      method: "POST",
+      body: JSON.stringify(ing),
+      headers: {"Content-type": "application/json"}
+    })
+      .then(response => response.json())
+        .then(responseData => setIngredients(prevIngredients => [
+            ...prevIngredients, 
+            {
+              ...ing, 
+              id: responseData.name
+            }
+          ])
+        );
   };
 
   const removeItemHandler = (id) => {
     setIngredients(prevIngredients => prevIngredients.filter(ing => ing.id !== id));
-  }
+  };
+
+  const filteredIngredientsHandler = useCallback(ings => setIngredients(ings), []);
 
   return (
     <div className="App">
       <IngredientForm addIngredient={addIngredientHandler}/>
-
       <section>
-        <Search />
+        <Search filteredIngredients={filteredIngredientsHandler}/>
         <IngredientsList ingredients={ingredients} onRemoveItem={removeItemHandler}/>
       </section>
     </div>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+
+  const [inputFilter, setInputFilter] = useState("");
+  const {filteredIngredients} = props;
+
+  useEffect(() => {
+    const queryParams = inputFilter.length === 0 
+      ? "" 
+      : `?orderBy="title"&equalTo="${inputFilter}"`;
+
+    fetch("https://react-hooks-practice-64697-default-rtdb.firebaseio.com/ingredients.json" + queryParams)
+    .then(response => response.json())
+    .then(responseData => {
+      const fetchedFilteredIngs = [];
+      for(let key in responseData)
+        fetchedFilteredIngs.push({
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+            id: key
+        });
+      filteredIngredients(fetchedFilteredIngs);
+    });
+  }, [inputFilter, filteredIngredients]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input 
+              type="text" 
+              value={inputFilter}
+              onChange={event => setInputFilter(event.target.value)}
+            />
         </div>
       </Card>
     </section>
